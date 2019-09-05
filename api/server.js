1990618312d5e768fab3142de0205cb238abef29
Adnan <adnan.chowdhury@outlook.com>
2019-09-05 11:42:54 -0500

    Project setup and dotenv installed and used

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const apiRouter = require('./api-router.js');
+const apiRouter = require("./api-router.js");
 
 const server = express();
 
+console.log("environment", process.env.NODE_ENV);
+
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 
 module.exports = server;
